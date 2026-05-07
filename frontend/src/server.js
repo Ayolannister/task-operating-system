b8409dbf6359feb1bcf062148b8338da8cf0e4e0
ayolannister <ayolannister@gmail.com>
2026-05-07 15:31:33 +0100

    added helm,containerized for k8s

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -9,6 +9,15 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "public")));
 
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
 app.listen(3000, () => {
   console.log("Frontend running on port 3000");
+});
+
+
+app.listen(5000, "0.0.0.0", () => {
+  console.log("API running on port 5000");
 });
