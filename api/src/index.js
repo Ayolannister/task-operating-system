39817210099841df801321558aab720f5b311efc
ayolannister <ayolannister@gmail.com>
2026-05-07 15:31:33 +0100

    added helm,containerized for k8s

--- api/src/index.js
+++ api/src/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import taskRoutes from "./routes/tasks.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/api", taskRoutes);
 
