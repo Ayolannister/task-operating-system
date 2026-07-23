3360ac820b1a728b2cb6de78b03181afae4d80e9
ayolannister <ayolannister@gmail.com>
2026-07-23 14:25:53 +0100

    Add Prometheus metrics endpoint

--- api/src/index.js
+++ api/src/index.js
@@ -1,9 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import client from "prom-client";
 import taskRoutes from "./routes/tasks.js";
 import cors from "cors";
 
 dotenv.config();
+client.collectDefaultMetrics();
+
+const register = client.register;
 
 const app = express();
 app.use(express.json());
@@ -14,6 +18,11 @@ app.use("/api", taskRoutes);
 app.get("/health", (req, res) => {
   res.send("OK");
 });
+app.get("/metrics", async (req, res) => {
+  res.set("Content-Type", register.contentType);
+  res.end(await register.metrics());
+});
+;
 
 const PORT = 5000;
 app.listen(PORT, () => {
